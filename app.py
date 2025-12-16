cb5a392640aa2a150250fe1fe22d70df010c7578
kyrill.borz <kyrill.borzenko@gmail.com>
2025-12-16 13:55:53 +0000

    functional first page with filter

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import sys
 import pyqtgraph as pg
+from scipy import signal as sg
+import polars as pl
+from functionality import apploadfilepolars, apply_butterworth_filter, filt_config
+from utils.utils import convertDfType
 from PyQt5.QtWidgets import (
     QApplication,
     QCheckBox,
@@ -11,7 +15,9 @@ from PyQt5.QtWidgets import (
     QLineEdit,
     QDialogButtonBox,
     QComboBox,
-    QPushButton
+    QPushButton,
+    QFileDialog,
+    QMessageBox
 )
 
 class Window(QWidget):
@@ -19,6 +25,8 @@ class Window(QWidget):
         super().__init__()
         pg.setConfigOption('background', '#f2f2f2')
         pg.setConfigOption('foreground', '#2a2a2a')
+        self.filt_config = filt_config
+        self.filt_config['butter']['Wn'] = filt_config['W']
 
         self.setWindowTitle("Processing GUI")
         self.resize(1200, 600)
@@ -36,27 +44,95 @@ class Window(QWidget):
 
         with open("style.css","r") as fh:
             self.setStyleSheet(fh.read())
+    def update_raw_plot(self):
+        self.plot_raw.clear()
 
+        y = self.data.original["ch_27"].to_numpy()[20000000:22000000]
+        x = range(len(y))
+        print(y)
+        print("plotting raw data")
+        self.plot_raw.plot(x, y, pen="b")
+    def update_filtered_plot(self):
+        self.plot_filtered.clear()
+        self.data.filter_ch = ["ch_27"]
+        signal2filter = self.data.original ###record.original #record.recording
+        self.data.apply_filter = 'butter_lowpass'
+        #config_text.append('signal2filter: %s' %signal2filter.name)
+        self.data.filter(signal2filter, self.data.apply_filter, **filt_config[self.data.apply_filter])
+        # Change from float64 to float 16
+        # self.data.filtered = convertDfType(self.data.filtered, typeFloat=pl.Float32)
+        y = self.data.filtered["ch_27"].to_numpy()[20000000:22000000]
+        x = range(len(y))
+        print(y)
+        self.plot_filtered.plot(x, y, pen="b")
+
+    def load_data(self):
+        path, _ = QFileDialog.getOpenFileName(
+            self,
+            "Select Data File",
+            "",
+            "Data Files (*.csv *.parquet *.txt);;All Files (*)"
+        )
+
+        if not path:
+            return
+
+        try:
+            self.data = apploadfilepolars(path)
+
+        except Exception as e:
+            QMessageBox.critical(self, "Load Error", str(e))
+            return
+
+        self.pathEdit.setText(path)
+
+        self.update_raw_plot()
+
+        if self.filterCheck.isChecked():
+            self.update_filtered_plot()
+    def clear_import_tab(self):
+        self.pathEdit.clear()
+        self.plot_raw.clear()
+        self.plot_filtered.clear()
+        self.data = None
+                
     def ImportTabUI(self):
-        """Create the General page UI."""
-        importTab = QWidget()
+        """Create the Import tab UI."""
+        self.importTab = QWidget()
         layout = QVBoxLayout()
-        formlayout = QFormLayout()
-        formlayout.addRow("Data Path:", QLineEdit())
-        formlayout.addRow("Filter:", QCheckBox())
-        formlayout.addRow("See Data:", QCheckBox())
-        plot_graph_raw = pg.PlotWidget()
-        plot_graph_filtered = pg.PlotWidget()
-        formlayout.addRow("Raw Signal:", plot_graph_raw)
-        formlayout.addRow("Filtered Signal:", plot_graph_filtered)
-        btnBox = QDialogButtonBox()
-        btnBox.setStandardButtons(
+        formLayout = QFormLayout()
+
+        # Data path
+        self.pathEdit = QLineEdit()
+        self.pathEdit.setReadOnly(True)
+        formLayout.addRow("Data Path:", self.pathEdit)
+
+        # Controls
+        self.filterCheck = QCheckBox()
+        self.seeDataCheck = QCheckBox()
+        formLayout.addRow("Filter:", self.filterCheck)
+        formLayout.addRow("See Data:", self.seeDataCheck)
+
+        # Plots
+        self.plot_raw = pg.PlotWidget(title="Raw Signal")
+        self.plot_filtered = pg.PlotWidget(title="Filtered Signal")
+        formLayout.addRow("Raw Signal:", self.plot_raw)
+        formLayout.addRow("Filtered Signal:", self.plot_filtered)
+
+        # Buttons
+        self.btnBox = QDialogButtonBox(
             QDialogButtonBox.Ok | QDialogButtonBox.Cancel
         )
-        layout.addLayout(formlayout)
-        layout.addWidget(btnBox)
-        importTab.setLayout(layout)
-        return importTab
+
+        layout.addLayout(formLayout)
+        layout.addWidget(self.btnBox)
+        self.importTab.setLayout(layout)
+
+        # Signal wiring
+        self.btnBox.accepted.connect(self.load_data)
+        self.btnBox.rejected.connect(self.clear_import_tab)
+
+        return self.importTab
 
     def NeuralTabUI(self):
         """Create the Network page UI."""
