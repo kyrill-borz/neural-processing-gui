1309577c20283a859660c5c78ac161168a26c60f
kyrill.borz <kyrill.borzenko@gmail.com>
2026-01-04 15:03:10 +0000

    referencing functionality

--- app.py
+++ app.py
@@ -44,16 +44,18 @@ class Window(QWidget):
 
         with open("style.css","r") as fh:
             self.setStyleSheet(fh.read())
-    def update_raw_plot(self):
-        self.plot_raw.clear()
+
+    def update_pg1_raw_plot(self):
+        self.plot_pg1_raw.clear()
 
         y = self.data.original["ch_27"].to_numpy()[20000000:22000000]
         x = range(len(y))
         print(y)
         print("plotting raw data")
-        self.plot_raw.plot(x, y, pen="b")
-    def update_filtered_plot(self):
-        self.plot_filtered.clear()
+        self.plot_pg1_raw.plot(x, y, pen="b")
+
+    def update_pg1_filtered_plot(self):
+        self.plot_pg1_filtered.clear()
         self.data.filter_ch = ["ch_27"]
         signal2filter = self.data.original ###record.original #record.recording
         self.data.apply_filter = 'butter_lowpass'
@@ -64,7 +66,7 @@ class Window(QWidget):
         y = self.data.filtered["ch_27"].to_numpy()[20000000:22000000]
         x = range(len(y))
         print(y)
-        self.plot_filtered.plot(x, y, pen="b")
+        self.plot_pg1_filtered.plot(x, y, pen="b")
 
     def load_data(self):
         path, _ = QFileDialog.getOpenFileName(
@@ -86,14 +88,15 @@ class Window(QWidget):
 
         self.pathEdit.setText(path)
 
-        self.update_raw_plot()
+        self.update_pg1_raw_plot()
 
         if self.filterCheck.isChecked():
-            self.update_filtered_plot()
+            self.update_pg1_filtered_plot()
+
     def clear_import_tab(self):
         self.pathEdit.clear()
-        self.plot_raw.clear()
-        self.plot_filtered.clear()
+        self.plot_pg1_raw.clear()
+        self.plot_pg1_filtered.clear()
         self.data = None
                 
     def ImportTabUI(self):
@@ -110,14 +113,16 @@ class Window(QWidget):
         # Controls
         self.filterCheck = QCheckBox()
         self.seeDataCheck = QCheckBox()
+        self.autofilterCheck = QCheckBox()
         formLayout.addRow("Filter:", self.filterCheck)
+        formLayout.addRow("Autofilter:", self.autofilterCheck)
         formLayout.addRow("See Data:", self.seeDataCheck)
 
         # Plots
-        self.plot_raw = pg.PlotWidget(title="Raw Signal")
-        self.plot_filtered = pg.PlotWidget(title="Filtered Signal")
-        formLayout.addRow("Raw Signal:", self.plot_raw)
-        formLayout.addRow("Filtered Signal:", self.plot_filtered)
+        self.plot_pg1_raw = pg.PlotWidget(title="Raw Signal")
+        self.plot_pg1_filtered = pg.PlotWidget(title="Filtered Signal")
+        formLayout.addRow("Raw Signal:", self.plot_pg1_raw)
+        formLayout.addRow("Filtered Signal:", self.plot_pg1_filtered)
 
         # Buttons
         self.btnBox = QDialogButtonBox(
@@ -134,50 +139,95 @@ class Window(QWidget):
 
         return self.importTab
 
+    def update_referencing_view(self):
+        self.plot_pg2_before.clear()
+        self.plot_pg2_after.clear()
+
+        # BEFORE
+        y_before = self.data.filtered[self.data.filter_ch[0]].to_numpy()
+        self.plot_pg2_before.plot(y_before)
+
+        # AFTER
+        y_after = self.data.referenced[self.data.filter_ch[0]].to_numpy()
+        self.plot_pg2_after.plot(y_after)
+
+    def apply_referencing(self):
+        method = self.refCombo.currentText()
+
+        if method in ("Select Referencing Method", "No Referencing"):
+            return
+
+        self.data.apply_referencing(method)
+        self.update_referencing_view()
+
     def NeuralTabUI(self):
-        """Create the Network page UI."""
-        NeuralTab = QWidget()
+        """Create the Neural Analysis tab UI."""
+        self.neuralTab = QWidget()
         layout = QGridLayout()
-        layout.addWidget(QCheckBox("Drop Bad Channels"), 1,0)
-        layout.addWidget(QCheckBox("Single Channel Analysis"),0,1)
-        layout.addWidget(QCheckBox("Multiple Channel Analysis"),0,2)
 
-        combobox = QComboBox(self)
-        combobox.addItem("Select Referencing Method")
-        combobox.addItem("No Referencing")
-        combobox.addItem("Median")
-        combobox.addItem("Laplacian")
-        combobox.addItem("Bipolar")
-        combobox.addItem("Tripolar")
-        layout.addWidget(combobox,0,0)
+        # Checkboxes
+        self.dropBadChCheck = QCheckBox("Drop Bad Channels")
+        self.singleChCheck = QCheckBox("Single Channel Analysis")
+        self.multiChCheck = QCheckBox("Multiple Channel Analysis")
 
-        combobox2 = QComboBox(self)
-        combobox2.addItem("Type of Single Channel Analysis")
-        combobox2.addItem("Referencing Only")
-        combobox2.addItem("Single Channel Spike Detection")
-        combobox2.addItem("ISI Distribution")
-        combobox2.addItem("Clustering of Spikes")
-        layout.addWidget(combobox2,1,1)
+        layout.addWidget(self.dropBadChCheck, 1, 0)
+        layout.addWidget(self.singleChCheck, 0, 1)
+        layout.addWidget(self.multiChCheck, 0, 2)
 
-        combobox3 = QComboBox(self)
-        combobox3.addItem("Type of Multi-Channel Analysis")
-        combobox3.addItem("Multiple Channel Spike Detection")
-        combobox3.addItem("Cross Correlation of Spike Trains")
-        combobox3.addItem("Directionality Analysis")
-        combobox3.addItem("Propogation Coefficient")
-        layout.addWidget(combobox3,1,2)
+        # Referencing selection
+        self.refCombo = QComboBox()
+        self.refCombo.addItems([
+            "Select Referencing Method",
+            "No Referencing",
+            "Median",
+            "Mean",
+            "Laplacian",
+            "Bipolar",
+            "Tripolar",
+        ])
+        layout.addWidget(self.refCombo, 0, 0)
 
-        plot_graph_raw = pg.PlotWidget()
-        plot_graph_filtered = pg.PlotWidget()
-        layout.addWidget(plot_graph_raw,2,0,1,3)
-        layout.addWidget(plot_graph_filtered,3,0,1,3)
-        btnBox = QDialogButtonBox()
-        btnBox.setStandardButtons(
+        # Analysis selectors
+        self.singleAnalysisCombo = QComboBox()
+        self.singleAnalysisCombo.addItems([
+            "Type of Single Channel Analysis",
+            "Referencing Only",
+            "Single Channel Spike Detection",
+            "ISI Distribution",
+            "Clustering of Spikes",
+        ])
+        layout.addWidget(self.singleAnalysisCombo, 1, 1)
+
+        self.multiAnalysisCombo = QComboBox()
+        self.multiAnalysisCombo.addItems([
+            "Type of Multi-Channel Analysis",
+            "Multiple Channel Spike Detection",
+            "Cross Correlation of Spike Trains",
+            "Directionality Analysis",
+            "Propagation Coefficient",
+        ])
+        layout.addWidget(self.multiAnalysisCombo, 1, 2)
+
+        # Plots (THIS is what you care about)
+        self.plot_pg2_before = pg.PlotWidget(title="Before Referencing")
+        self.plot_pg2_after = pg.PlotWidget(title="After Referencing")
+
+        layout.addWidget(self.plot_pg2_before, 2, 0, 1, 3)
+        layout.addWidget(self.plot_pg2_after, 3, 0, 1, 3)
+
+        # Buttons
+        self.neuralBtnBox = QDialogButtonBox(
             QDialogButtonBox.Ok | QDialogButtonBox.Cancel
         )
-        layout.addWidget(btnBox,4,2)
-        NeuralTab.setLayout(layout)
-        return NeuralTab
+        layout.addWidget(self.neuralBtnBox, 4, 2)
+
+        self.neuralTab.setLayout(layout)
+
+        # Signal wiring
+        self.refCombo.currentTextChanged.connect(self.update_referencing_view)
+        self.neuralBtnBox.accepted.connect(self.apply_referencing)
+
+        return self.neuralTab
     
     def ProcessingTabUI(self):
         """Create the Network page UI."""
