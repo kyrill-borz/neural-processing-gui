d5e1076b79e0c0a688b8f8e179a84bc6c199f615
kyrill.borz <kyrill.borzenko@gmail.com>
2026-04-16 21:54:10 +0100

    channel name change

--- controller.py
+++ controller.py
@@ -38,7 +38,10 @@ class AppController:
 
     def make_channel_names(self):
         for i, channel in enumerate(self.data.filter_ch):
-            self.channel_names[channel] = channel.replace("ch_", "Channel ") + (f" ({round(self.data.information['Z_magnitude_KOhms'].iloc[i],3)})" if self.data.information is not [] else " kOhms")
-    
+            try:
+                self.channel_names[channel] = channel.replace("ch_", "Channel ") + (f" ({round(self.data.information['Z_magnitude_KOhms'].iloc[i],3)})" if self.data.information is not [] else " kOhms")
+            except:
+                self.channel_names[channel] = channel.replace("ch_", "Channel ")
+                
     def get_channel_name(self, channel):
         return self.channel_names[channel]
